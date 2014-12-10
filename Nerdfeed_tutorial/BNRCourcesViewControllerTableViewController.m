dc2fedc0d40598989d78b62e32337718ef8915fc
Hiro51 <h.tsujino0714@gmail.com>
2014-12-10 17:11:09 +0900

    create a HTTP request and fetch the data as JSON

--- Nerdfeed_tutorial/BNRCourcesViewControllerTableViewController.m
+++ Nerdfeed_tutorial/BNRCourcesViewControllerTableViewController.m
@@ -10,6 +10,8 @@
 
 @interface BNRCourcesViewControllerTableViewController ()
 
+@property (nonatomic) NSURLSession *session;
+
 @end
 
 @implementation BNRCourcesViewControllerTableViewController
@@ -20,6 +22,29 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     return nil;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self){
+        self.navigationItem.title = @"BNR Courses";
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
+        [self fetchFeed];
+    }
+    return self;
+}
+
+- (void)fetchFeed {
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", json);
+    }];
+    [dataTask resume];
 }
 
 @end
